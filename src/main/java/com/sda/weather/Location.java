6b7cc062ca4d6d7f3d9dcc62bbd6f73abf80d616
Piotr Białobrzeski <p.d.bialobrzeski@gmail.com>
2021-04-25 19:50:16 +0200

    added Location class updated LocationController added hibernate and mySQL dependencies in pom.xml

---
+++ src/main/java/com/sda/weather/Location.java
@@ -0,0 +1,79 @@
+package com.sda.weather;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Location {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String city;
+    String region;
+    String country;
+    int longitude;
+    int altitude;
+
+    public Location() {
+    }
+
+    public Location(Long id, String city, String region, String country, int longitude, int altitude) {
+        this.id = id;
+        this.city = city;
+        this.region = region;
+        this.country = country;
+        this.longitude = longitude;
+        this.altitude = altitude;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public int getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(int longitude) {
+        this.longitude = longitude;
+    }
+
+    public int getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(int altitude) {
+        this.altitude = altitude;
+    }
+}
