6d25775e24f582b976c361ee2db224a5232eae2a
Michal Paukszto <michal.paukszto@gmail.com>
2021-04-25 15:50:14 +0200

    remove credentials

--- src/main/java/com/sda/weather/WeatherApplication.java
+++ src/main/java/com/sda/weather/WeatherApplication.java
@@ -3,9 +3,7 @@ package com.sda.weather;
 public class WeatherApplication {
 
     public static void main(String[] args) {
-
         UserInterface userInterface = new UserInterface();
         userInterface.runApplication();
-
     }
 }
